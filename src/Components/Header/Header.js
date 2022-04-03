1636357214cd5ab36e44a02cdc0884911c73a30e
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-04-04 02:38:49 +0600

    readme.md is added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import "./Header.css";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <h1>Welcome to React Route</h1>
             <div className='link'>
-                <Link to="/">Home</Link>
-                <Link to="about">About</Link>
-                <Link to="users">Users</Link>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="about">About</NavLink>
+                <NavLink to="users">Users</NavLink>
             </div>
         </div>
     );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
+    const getUsers = () => {
+        navigate("/users")
+    }
     return (
         <div>
             <h1>This is my Home....</h1>
             <h3>Dei ekhane ghm...</h3>
+            <button onClick={getUsers}>Users</button>
         </div>
     );
 };
